b783284851a19b45bda9091362dc686397053be8
Elbatis <pintammarco@gmail.com>
2026-03-22 11:52:59 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+const imagenes = document.querySelectorAll(".work img");
+const lightbox = document.getElementById("lightbox");
+const imgGrande = document.getElementById("img-grande");
+const cerrar = document.getElementById("cerrar");
+
+imagenes.forEach(img => {
+  img.addEventListener("click", () => {
+    lightbox.style.display = "flex";
+    imgGrande.src = img.src;
+  });
+});
+
+cerrar.addEventListener("click", () => {
+  lightbox.style.display = "none";
+});
